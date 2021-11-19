47d1b4bf0268a485e1f217994a3fe2fb97e190ca
GitJN-1 <Nicolaschavez3620@gmail.com>
2021-11-18 20:40:09 -0500

    actualizar querys

---
+++ querys/HU_015.ts
@@ -0,0 +1,28 @@
+import conectarBD from "../db/db";
+import { UserModel } from "../models/user";
+import { Enum_InscriptionStatus } from "../models/enums";
+import { ProjectModel } from "../models/project";
+import { InscriptionModel } from '../models/inscriptions';
+
+const main = async () => {
+    await conectarBD();
+
+// OBTENER LAS SOLICITUDES
+    await InscriptionModel.find({ project : "619685784fac5f8c7037f545" })
+      .then((u) => {
+        console.log('solicitudes de inscripción encontradas', u);
+      })
+      .catch((e) => {
+        console.error('error al buscar la solicitud de inscripción', e);
+      });
+
+};
+
+main();
+
+
+// Historia de usuario: HU_015
+// Como líder
+// Dado que ingresé al sistema de gestión de proyectos
+// Cuando requiera revisar las solicitudes pendientes por aceptar o rechazar de estudiantes de inscripción a mis proyectos
+// Entonces podré listar las solicitudes realizadas por los estudiantes.
