86f5da69ff075a514e0cff802ef7477d706ad1ed
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-11-18 20:59:13 -0500

    Update enums.ts
    update Enums

--- models/enums.ts
+++ models/enums.ts
@@ -7,55 +7,35 @@ enum Enum_Rol {
 enum Enum_EstadoUsuario {
   pendiente = 'pendiente',
   autorizado = 'autorizado',
-  no_autorizado = 'no_autorizado',
+  noAutorizado = 'noAutorizado',
 }
   
-  enum Enum_ProjectStatus {
-    ACTIVO = 'ACTIVO',
-    INACTIVO = 'INACTIVO',
-  }
-  
-  enum Enum_ProjectPhase {
-    INICIADO = 'INICIADO',
-    DESARROLLO = 'DESARROLLO',
-    TERMINADO = 'TERMINADO',
-    NULO = '',
-  }
-  
-  enum Enum_ObjectiveType {
-    GENERAL = 'GENERAL',
-    ESPECIFICO = 'ESPECIFICO',
-  }
-  
-  enum Enum_InscriptionStatus {
+  enum Enum_EstadoInscripcion {
     aceptada = 'aceptada',
     rechazada = 'rechazada',
   }
 
   enum Enum_EstadoProyecto {
-    ACTIVO = 'ACTIVO',
-    INACTIVO = 'INACTIVO',
+    activo = 'activo',
+    inactivo = 'inactivo',
   }
   
   enum Enum_FaseProyecto {
-    INICIADO = 'INICIADO',
-    DESARROLLO = 'DESARROLLO',
-    TERMINADO = 'TERMINADO',
-    NULO = '',
+    iniciado = 'iniciado',
+    desarrollo = 'desarrollo',
+    terminado = 'terminado',
+    nulo = '',
   }
   
   enum Enum_TipoObjetivo {
-    GENERAL = 'GENERAL',
-    ESPECIFICO = 'ESPECIFICO',
+    general = 'general',
+    especifico = 'especifico',
   }
   
   export {
     Enum_Rol,
     Enum_EstadoUsuario,
-    Enum_ProjectStatus,
-    Enum_ProjectPhase,
-    Enum_ObjectiveType,
-    Enum_InscriptionStatus,
+    Enum_EstadoInscripcion,
     Enum_EstadoProyecto, 
     Enum_FaseProyecto,
     Enum_TipoObjetivo
