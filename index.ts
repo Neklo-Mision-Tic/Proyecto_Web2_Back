58a5457410dfc8ebba8a8db71ef575758fc8749c
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-11-19 20:29:42 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const main = async () => {
     //   apellido: "Mina",
     //   correo: "test3@neklo.com",
     //   identificacion: 136,
-    //   rol: Enum_Rol.Estudiante,
+    //   rol: Enum_Rol.estudiante,
     // })
 
     //   .then((u) => {
@@ -37,16 +37,16 @@ const main = async () => {
   const proyectoCreado =  ProjectModel.create({
     nombre: 'Proyecto Mision TIC2',
     objetivos: [
-      { descripcion: 'Este es el objetivo general', tipo: Enum_TipoObjetivo.GENERAL },
-      { descripcion: 'Este es el objetivo especifico 1', tipo: Enum_TipoObjetivo.ESPECIFICO },
-      { descripcion: 'Este es el objetivo especifico 2', tipo: Enum_TipoObjetivo.ESPECIFICO },
+      { descripcion: 'Este es el objetivo general', tipo: Enum_TipoObjetivo.general },
+      { descripcion: 'Este es el objetivo especifico 1', tipo: Enum_TipoObjetivo.especifico },
+      { descripcion: 'Este es el objetivo especifico 2', tipo: Enum_TipoObjetivo.especifico },
     ],
     presupuesto: 120000,
     fechaInicio: new Date('2021/12/24'),
     fechaFin: new Date('2022/12/24'),
     lider: "6195d905beaedc9b1a520181",
-    estado: Enum_EstadoProyecto.ACTIVO,
-    fase: Enum_FaseProyecto.INICIADO
+    estado: Enum_EstadoProyecto.activo,
+    fase: Enum_FaseProyecto.iniciado
     
   });
 
