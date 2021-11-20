4aa098db20f4988ab7076fc96a3ed9fd6cbe675d
Lissette Sierra <90323474+Amozalita@users.noreply.github.com>
2021-11-19 22:08:53 -0500

    Update HU_002.ts

---
+++ querys/HU_002.ts
@@ -0,0 +1,12 @@
+await UserModel.find({
+    $and: [
+      { correo: "test10@neklo.com" },
+      { password: "test123" },
+    ],
+  })
+    .then((u) => {
+      console.log("Usuario autenticado", u);
+    })
+    .catch((e) => {
+      console.error("Error al listar usuarios", e);
+    });
