3def7c43928fa7c3672dddd009394d83beb00b1c
Osbaldo Cortes L <81762576+ocortesl@users.noreply.github.com>
2021-11-19 22:19:38 -0500

    Update HU_001.ts
    Actualización historia.

---
+++ querys/HU_001.ts
@@ -0,0 +1,10 @@
+  //Ingresar los datos de registro (Incluyendo elegir el rol al que aspiro) HU_01
+
+    // await UserModel.create({
+    //   correo: "test11@neklo.com",
+    //   identificacion: 1345,
+    //   password: "test124",
+    //   nombre: "Gabriella",
+    //   apellido: "Linares",      
+    //   rol: enum_Rol.estudiante,
+    // })
