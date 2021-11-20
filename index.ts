f8ecf5c621f592a2a15b84fa67f20f21d13cdf84
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-11-19 21:12:32 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -60,33 +60,3 @@ const main = async () => {
 };
 
 main();
-
-
-// import conectarBD from "./db/db";
-// import { Enum_EstadoInscripcion } from "./models/enums";
-// import { InscriptionModel } from './models/inscription';
-// import { ProjectModel } from "./models/project";
-// import { UserModel } from "./models/user";
-
-// const main = async () => {
-//   await conectarBD();
-
-// // GENERAR UNA SOLICITUD
-//   await InscriptionModel.create({
-//     estado : Enum_EstadoInscripcion.pendiente,
-//     fechaIngreso : new Date('2021/12/25'),
-//     fechaEgreso : new Date('2022/10/25'),
-//     proyecto : "6197b66585646e5844bab56c",
-//     estudiante: "6195d905beaedc9b1a520181"
-    
-//   })
-//   .then((u) => {
-//     console.log('solicitud de inscripción generada exitosamente', u);
-//   })
-//   .catch((e) => {
-//     console.error('error al generar la solicitud', e);
-//   });
-
-// };
-
-// main();
