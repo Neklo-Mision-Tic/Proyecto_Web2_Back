7d1682df3dc888c9c83f35aa84c7fd4b0b5ac95c
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-12-20 16:35:38 -0500

    Add files via upload

---
+++ models/incripcion/resolver.js
@@ -0,0 +1,39 @@
+import { InscriptionModel } from "./isncripcion.js"
+
+const resolverInscripciones={
+    Query:{
+        Inscripciones:async(parent,args)=>{
+            const inscripciones=await InscriptionModel.find().populate('proyecto').populate('estudiante');
+            return inscripciones;
+        },
+        inscripcionByEstudiante:async(parent,args)=>{
+            const inscripciones=await InscriptionModel.find({estudiante:args.estudiante}).populate('estudiante').populate('proyecto'); 
+            return inscripciones;
+        },
+    },
+    Mutation:{
+        crearInscripcion:async(parent,args)=>{
+            const minscripcion=await InscriptionModel.create({
+                estado:args.estado,
+                proyecto:args.proyecto,
+                estudiante:args.estudiante
+            });
+            return minscripcion;
+        },
+        aprobarInscripcion:async(parent,args)=>{
+            const inscripcionaprobada=await InscriptionModel.findByIdAndUpdate({_id:args._id},{
+                estado:'ACEPTADO',
+                fechaIngreso:Date.now(),
+            },{new:true});
+            return inscripcionaprobada;
+        },
+        rechazarInscripcion:async(parent,args)=>{
+            const inscripcionrechazada=await InscriptionModel.findByIdAndUpdate({_id:args._id},{
+                estado:'RECHAZADO',
+                fechaEgreso:Date.now(),
+            },{new:true});
+            return inscripcionrechazada;
+        },
+    }
+}
+export {resolverInscripciones};
